b96c3e26cebcf7761412f48e15c493c876772e59
jsonccolu <jsonccolu@unsa.edu.pe>
2020-05-20 19:15:38 -0500

    Operator Point for Web

---
+++ ImageProcessing/ImageProcessing/pointOperator/pointOperator.py
@@ -0,0 +1,27 @@
+
+
+class pointOperator:
+    def __init__(self,_img):
+        self.img=_img
+
+    def thresHolding(self):
+        """Codigo de thresholding"""
+
+    def contrastStretching(self):
+        """codigo de contrastStret"""
+
+    def histogramEqualizer(self):
+        """codigo"""
+
+    def logarithmOperator(self):
+        """codigo"""
+
+    def raizOperator(self):
+        """codigo"""
+
+    def exponentialOperator(self):
+        """codigo"""
+
+    def raisepowerOperator(self):
+        """codigo"""
+
